13d735f802f26ba91c43f19f8a65419660f79fd2
karol cetnar <karolcetnar84@gmail.com>
2022-12-07 16:10:40 +0100

    themeChanger
    test2

--- theme.js
+++ theme.js
@@ -1,10 +1,12 @@
 const zmien = document.getElementById('toggleSun');
 const card = document.getElementById('zmianaKoloru');
 const overprice = document.getElementById('jeb');
+const button = document.getElementById('button1')
 
 const h4 = document.querySelector('h4'); 
 const p = document.querySelector('p'); 
 const body = document.querySelector('body'); 
+
 var moon = 'icon-moon';
 var sun = 'icon-sun'; 
 zmien.addEventListener('click', function(){
@@ -18,13 +20,15 @@ zmien.addEventListener('click', function(){
     }
     
     if(this.classList.contains(sun)){
-        body.style.background = 'rgb(44, 43, 43)';
+        body.style.background = '#303841';
         body.style.color = "white"
-        card.style.backgroundColor = 'rgb(83, 81, 81)';
+        card.style.backgroundColor = '#3A4750';
         overprice.style.color = 'rgb(238, 238, 238)';
         h4.style.color = 'rgb(238, 238, 238)';
         p.style.color = 'rgb(238, 238, 238)';
         body.style.transition = '1s';
+        button.style.background = '#00ADB5';
+  
     }
     else
     {
@@ -34,8 +38,9 @@ zmien.addEventListener('click', function(){
         overprice.style.color = 'hsl(228, 12%, 48%)';
         h4.style.color = 'hsl(228, 12%, 48%)';
         p.style.color = 'hsl(228, 12%, 48%)';
-        
+        button.style.background = 'hsl(158, 36%, 37%)';
         body.style.transition = '1s';
+        
 
     }
     
